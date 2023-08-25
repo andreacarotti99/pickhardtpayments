dc21a3c6d5c3d0376826e79a15e297150e2b2bcd
andreacarotti <34317356+andreacarotti99@users.noreply.github.com>
2023-08-25 13:41:50 +0200

    update

---
+++ fork/equilibrium/equilibrium.py
@@ -0,0 +1,29 @@
+from pickhardtpayments.fork.Simulation import Simulation
+from pickhardtpayments.pickhardtpayments import ChannelGraph
+from pickhardtpayments.fork.ExportResults import ExportResults
+
+
+base = 20_000
+snapshot_path = '/Users/andreacarotti/Desktop/LN/_PickhardtPayments/pickhardtpayments/fork/SNAPSHOTS/cosimo_19jan2023_converted.json'
+channel_graph = ChannelGraph(snapshot_path)
+channel_graph.transform_channel_graph_to_simpler(
+    tentative_nodes_to_keep=1000, strategy="weighted_by_capacity")
+s = Simulation(channel_graph, base)
+s.run_success_payments_simulation(
+    payments_to_simulate=10,
+    payments_amount=10_000,
+    mu=100,
+    base=base,
+    distribution="weighted_by_capacity",
+    dist_func="linear",
+    verbose=False
+)
+
+
+
+export = ExportResults(simulation=s)
+export.export_results()
+
+
+
+
