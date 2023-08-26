0b445a3c589161c10aec28109911b8c8b2115796
andreacarotti <34317356+andreacarotti99@users.noreply.github.com>
2023-08-26 15:07:13 +0200

    step by step process

--- fork/equilibrium/equilibrium.py
+++ fork/equilibrium/equilibrium.py
@@ -3,6 +3,14 @@ from pickhardtpayments.pickhardtpayments import ChannelGraph
 from pickhardtpayments.fork.ExportResults import ExportResults
 
 
+# Testing if the LN has reached a fee equilibrium:
+# 1) Deciding parameters of the simulation: num. of nodes to keep, strategy for choosing nodes, payment amount, number of payments, distribution of payments
+# 2) Running a simulation, obtaining some fees for every node
+# 3) Randomly picking a node
+# 4) Changing the channels of the node with other channels of the same capacity (starting randomly then using some strategy)
+# 5) Running a new simulation, obtaining some fees for every node and specifically for the changed node
+# 6) Repeating 3), 4) and 5) 10 times and observe the difference in fee earned by the randomly picked node
+
 base = 20_000
 snapshot_path = '/Users/andreacarotti/Desktop/LN/_PickhardtPayments/pickhardtpayments/fork/SNAPSHOTS/cosimo_19jan2023_converted.json'
 channel_graph = ChannelGraph(snapshot_path)
